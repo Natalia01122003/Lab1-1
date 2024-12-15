55a383a9fd511cffdeb54331e90fd32ff0bdcdcc
Natalia01122003 <n.a.t.a.l.i.a.01v12v2003@gmail.com>
2024-12-16 00:16:23 +0700

    Реализованная программа

---
+++ testLab3/app/src/main/java/com/example/testlab3/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.testlab3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val text: TextView = findViewById(R.id.text)
+        val button: Button = findViewById(R.id.button)
+        val resultText: TextView = findViewById(R.id.resultText)
+
+        button.setOnClickListener {
+            val inputText = text.text.toString()
+            val count = countLatinLetters(inputText)
+            resultText.text = "Количество латинских букв: $count"
+        }
+    }
+
+    private fun countLatinLetters(input: String): Int {
+        return input.count { it.isLetter() && it.toChar().isLetter() && (it in 'A'..'Z' || it in 'a'..'z') }
+    }
+}
